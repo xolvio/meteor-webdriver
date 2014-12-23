2d3af70272e94c252197f56745e70d753de00a03
sam@hatoum.net <sam@hatoum.net>
2014-12-22 17:29:54 -0700

    bumping version

--- package.js
+++ package.js
@@ -5,7 +5,7 @@
   Package.describe({
     name: 'xolvio:webdriver',
     summary: 'Webdriver.io for Meteor',
-    version: '0.1.0',
+    version: '0.1.1',
     git: 'git@github.com:xolvio/meteor-webdriver.git',
     debugOnly: true
   });
